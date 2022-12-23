79fe5808c059623fde53c6cf3e5443eac0fa434b
Paul Kinlan <paul.kinlan@gmail.com>
2022-12-23 21:40:08 +0000

    removing normalization that's not used

---
+++ ml/README.md
@@ -0,0 +1,1 @@
+This is just a backup of the training script I've used.
--- ml/train.py
+++ ml/train.py
@@ -64,15 +64,6 @@ for image_batch, labels_batch in train_ds:
   print(labels_batch.shape)
   break
 
-# Normalize the data
-normalization_layer = tf.keras.layers.Rescaling(1./255)
-
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
-# Notice the pixel values are now in `[0,1]`.
-print(np.min(first_image), np.max(first_image))
-
 AUTOTUNE = tf.data.AUTOTUNE
 
 train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
