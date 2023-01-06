2b2c25e83972f7df3665cf120fbbe6b9806bae29
Paul Kinlan <paul.kinlan@gmail.com>
2023-01-06 12:22:43 +0000

    Generating images with png and jpeg.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer";
 import minimist from "minimist";
 import { v4 as uuidv4 } from "uuid";
+import sharp from "sharp";
 
 const log = (str) => process.stdout.write(str);
 
@@ -17,6 +18,15 @@ async function navigate(browser, url) {
     if (page != null) page.close();
     return null;
   }
+}
+
+function saveScreenshot(buffer, uuid, className, state) {
+  const image = sharp(buffer);
+  image.clone().png().toFile(`./data/images/${className}/${uuid}-${state}.png`);
+  image
+    .clone()
+    .jpeg({ quality: 25 })
+    .toFile(`./data/images/${className}/${uuid}-${state}.jpeg`);
 }
 
 async function extractLinks(page) {
@@ -58,104 +68,12 @@ async function extractLinks(page) {
       if (rect.width == 0 || rect.height == 0) continue; // skip small links (like the ones in the footer) - 50 because we added 20 to the width and height and 30 is probably the smallest link
 
       const uuid = uuidv4();
-      output.push([
-        await page.screenshot({
-          clip: rect,
-          path: `./data/images/text-links/${uuid}.png`,
-        }),
-      ]);
-    }
-  }
 
-  return output;
-}
-
-async function extractImageLinks(page) {
-  // Extract the results from the page.
-  const anchors = await page.$$("a:has(img)"); // image links only
-  const output = [];
-
-  for (const anchor of anchors) {
-    const bounding = await anchor.boundingBox();
-    if (bounding != null) {
-      const rect = await page.evaluate((el) => {
-        function isOccluded(element) {
-          const { x, y, width, height } = element.getBoundingClientRect();
-
-          // We inset 5px to avoid the edges of the button.
-          const elementsTopLeft = document.elementsFromPoint(x + 5, y + 5);
-          const elementsTopRight = document.elementsFromPoint(
-            x + width - 5,
-            y + 5
-          );
-          const elementsBottomLeft = document.elementsFromPoint(
-            x + 5,
-            y + height - 5
-          );
-          const elementsBottomRight = document.elementsFromPoint(
-            x + width - 5,
-            y + height - 5
-          );
-
-          if (
-            elementsTopLeft.indexOf(element) == -1 ||
-            elementsTopRight.indexOf(element) == -1 ||
-            elementsBottomLeft.indexOf(element) == -1 ||
-            elementsBottomRight.indexOf(element) == -1
-          ) {
-            return true;
-          }
-
-          return false;
-        }
-
-        el.scrollIntoView({
-          behavior: "auto",
-          block: "center",
-          inline: "center",
-        });
-
-        if (isOccluded(el)) {
-          return { width: 0, height: 0 };
-        }
-
-        const { x, y, width, height } = el.getBoundingClientRect();
-        const elements = document.elementsFromPoint(
-          x + width / 2,
-          y + height / 2
-        );
-
-        // We only want the link where it is in the chain of elements. (likely visible from the current point.)
-        if (elements.indexOf(el) > -1) {
-          return {
-            x: Math.max(window.scrollX + x - 10, 0),
-            y: Math.max(window.scrollY + y - 10, 0),
-            width: width + 20,
-            height: height + 20,
-          };
-        }
-        // We ignore these later.
-        return { width: 0, height: 0 };
-      }, anchor);
-
-      if (rect.width == 0 || rect.height == 0) continue; // skip small links (like the ones in the footer) - 50 because we added 20 to the width and height and 30 is probably the smallest link
-
-      const uuid = uuidv4();
-      output.push([
-        await page.screenshot({
-          clip: rect,
-          path: `./data/images/image-links/${uuid}.png`,
-        }),
-      ]);
-
-      await anchor.hover();
-
-      output.push([
-        await page.screenshot({
-          clip: rect,
-          path: `./data/images/image-links/${uuid}-hover.png`,
-        }),
-      ]);
+      const screenshot = await page.screenshot({
+        clip: rect,
+      });
+      output.push(screenshot);
+      saveScreenshot(screenshot, uuid, "text-links", "normal");
     }
   }
 
@@ -176,7 +94,10 @@ async function extractButtons(page) {
           const padding = 10;
 
           // We inset {padding}px to avoid the edges of the button.
-          const elementsTopLeft = document.elementsFromPoint(x + padding, y + padding);
+          const elementsTopLeft = document.elementsFromPoint(
+            x + padding,
+            y + padding
+          );
           const elementsTopRight = document.elementsFromPoint(
             x + width - padding,
             y + padding
@@ -235,21 +156,21 @@ async function extractButtons(page) {
       if (rect.width == 0 || rect.height == 0) continue; // skip small buttons (like the ones in the footer
 
       const uuid = uuidv4();
-      output.push([
-        await page.screenshot({
-          clip: rect,
-          path: `./data/images/buttons/${uuid}.png`,
-        }),
-      ]);
+
+      const screenshot = await page.screenshot({
+        clip: rect,
+      });
 
       await button.hover();
 
-      output.push([
-        await page.screenshot({
-          clip: rect,
-          path: `./data/images/buttons/${uuid}-hover.png`,
-        }),
-      ]);
+      const screenshotHover = await page.screenshot({
+        clip: rect,
+      });
+
+      output.push(screenshot);
+      output.push(screenshotHover);
+      saveScreenshot(screenshot, uuid, "buttons", "normal");
+      saveScreenshot(screenshotHover, uuid, "buttons", "hover");
     }
   }
 
@@ -260,10 +181,13 @@ async function get(urls = [], browser) {
   const results = { allButtons: {}, allLinks: {}, allImageLinks: {} };
   for (let urlIdx = 0; urlIdx < urls.length; urlIdx++) {
     let line = urls[urlIdx];
-    let [filter, url] = line.split(/:(.*)/)
+    let [filter, url] = line.split(/:(.*)/);
     // We should check for formatting errors.
 
-    let extract = { button: filter.includes("button"), link: filter.includes("link") };
+    let extract = {
+      button: filter.includes("button"),
+      link: filter.includes("link"),
+    };
 
     log(`Fetching ${urlIdx + 1}/${urls.length} ${url}`);
 
@@ -271,7 +195,7 @@ async function get(urls = [], browser) {
 
     if (page != null) {
       try {
-        if (extract.button) { 
+        if (extract.button) {
           results.allButtons[url] = await extractButtons(page);
         }
         if (extract.link) {
@@ -279,7 +203,9 @@ async function get(urls = [], browser) {
         }
 
         log(
-          `. Done. Buttons: ${results.allButtons[url]?.length || 0}, Links: ${results.allLinks[url]?.length || 0}\n`
+          `. Done. Buttons: ${results.allButtons[url]?.length || 0}, Links: ${
+            results.allLinks[url]?.length || 0
+          }\n`
         );
       } catch (e) {
         log(`. Failed.`);
